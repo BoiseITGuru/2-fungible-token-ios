9935b3beff153403ea3693eb71faaeac6cfffe39
BoiseITGuru <dev@boiseitguru.com>
2023-11-15 11:51:15 -0700

    repo init

---
+++ Fungible Token/Fungible_TokenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fungible_TokenApp.swift
+//  Fungible Token
+//
+//  Created by BoiseITGuru on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Fungible_TokenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
